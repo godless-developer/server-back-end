157dbc073bb8ce6f78e9b1391209617fb1027ca5
godless-developer <bbtulga98@gmail.com>
2025-02-11 12:13:12 +0800

    change

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const { upComingMovies } = require("./utils/movieUpComing");
 const app = express();
+app.use(express.json());
 const cors = require("cors");
 app.use(cors());
+const bodyParser = require("body-parser");
+app.use(bodyParser());
 
-//popular
-app.get("/", (req, res) => {
-  res.send({
+let movies = [];
+
+const addMovies = (movieData) => {
+  movies.push(movieData);
+  console.log(movies);
+};
+app.get("/", (request, response) => {
+  response.send({
     data: upComingMovies,
+    text: "hello world",
   });
+});
+
+app.post("/movie", (request, response) => {
+  console.log("body!", request.body);
+  addMovies(request.body);
+  response.send("Movie succesfully added ");
 });
 
 app.listen(4000, () => {
